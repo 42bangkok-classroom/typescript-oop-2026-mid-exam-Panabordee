c703de8d4521fe0289bc770f793bbc3e842c3dfb
Panabordee <68010697@kmitl.ac.th>
2026-01-21 15:53:16 +0700

    po6 uinq

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -2,15 +2,8 @@ const arr1 : number[] = [1, 2, 3, 4];
 const arr2 : number[]= [3, 4, 5, 6];
 
 export function getUniqueNumbers(arr1:number[], arr2:number[]): number[] {
-
-  
   let arr = [...arr1, ...arr2];
-
-  
   let uniqueArr = [...new Set(arr)];
   console.log(uniqueArr);
-  
-
-
 return uniqueArr
 }
