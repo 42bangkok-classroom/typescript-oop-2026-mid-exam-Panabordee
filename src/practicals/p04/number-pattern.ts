72d868461ca4e9d2c575da8783e077d477d060d4
Panabordee <68010697@kmitl.ac.th>
2026-01-21 14:20:56 +0700

    test1

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,13 @@
-// Write your code below
+const p =Number(process.argv[2]);
+if (!isNaN(p) && p >0) {
+for (let i = 1 ;i <=p; i++) {
+console.log(i)
+    let row ="";
+
+for (let j = p; j >= 1; j--) {
+    row += j
+;
+    }
+console.log(row);
+  }
+}   
--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,7 @@
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
+  let A = console.log("sadasd")
+  return A;
   // Write your code below
 }
