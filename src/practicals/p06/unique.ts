1381475250f925895a4e0bbf051607cfee7cbf05
Panabordee <68010697@kmitl.ac.th>
2026-01-21 15:00:36 +0700

    test3

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,15 +1,15 @@
 const arr1 : number[] = [1, 2, 3, 4];
 const arr2 : number[]= [3, 4, 5, 6];
 
-function getUniqueAfterMerge(arr1:number[], arr2:number[]): number[] {
+function getUniqueNumbers(arr1:number[], arr2:number[]): number[] {
 
-  // merge two arrays
+  
   let arr = [...arr1, ...arr2];
 
-  // removing duplicate
+  
   let uniqueArr = [...new Set(arr)];
-
   console.log(uniqueArr);
+  
 
 
 return uniqueArr
