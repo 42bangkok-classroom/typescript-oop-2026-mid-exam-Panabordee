b8a407cd8729aeebe03f946bcb8a66110642b184
Panabordee <68010697@kmitl.ac.th>
2026-01-21 16:16:35 +0700

    clean code

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -3,8 +3,8 @@ if ((!number  || number <= -1) && number !== 0) {
   process.exit();
 }
 let row = "";
-for (let index = 0; index < number; index++) {
-  let num = index + 1;
+for (let i = 0; i < number; i++) {
+  let num = i + 1;
   console.log(`${" ".repeat(number - num)}${num}${row}`);
   row = `${num}${row}`;
 }
